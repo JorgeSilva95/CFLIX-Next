682950588f0f946894074765d23fac2b7c9ee855
JorgeSilva95 <jorgesilva010995@gmail.com>
2023-07-04 21:39:49 -0300

    png

--- Components/Perfs.jsx
+++ Components/Perfs.jsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 import Perfss from "../Styls/Perfs.module.css";
 import { BiLock } from "react-icons/bi";
 import DBn from "../DB/DB.json";
+import url from "../DB/Imgs/Perfil1.png";
+import Image from "next/image";
 export default function Perfs() {
   const namesL = DBn;
   const namesLs = namesL.Users;
-
-  const url = "../DB/Imgs/Perfil1.png";
   return (
     <div className={Perfss.perfCard}>
       <Link href="/home">
@@ -15,7 +15,7 @@ export default function Perfs() {
             alert("Seja Bem-Vindo!!");
           }}
         >
-          <img srcSet={url} alt="Perfil" />
+          <Image src={url} alt="Perfil" />
           <p>perfil</p>
         </button>
       </Link>
