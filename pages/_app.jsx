27d719cc63398e30b598fe350d69168a32c7aebb
JorgeSilva95 <jorgesilva010995@gmail.com>
2024-06-11 10:41:11 -0300

    fix: correction styles and add roboto font

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,14 @@
-import "../Styles/index.css";
+import "../Styles/global.css";
 
 export default function dev({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+      <title>CCFLIX - Busque seus Filmes</title>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
